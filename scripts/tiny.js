ebb436c0bbeb09748e197c8c28afbc791eb48852
wangjianfeng03 <wangjianfeng03@meituan.com>
2018-10-28 13:28:18 +0800

    first

---
+++ scripts/tiny.js
@@ -0,0 +1,54 @@
+// Description:  
+//   This is a tinypic.  
+// Commands:  
+//   okbot helo - Reply with world!  
+
+const bearychat = require('bearychat');
+var fs = require("fs");
+var process = require("process");
+var tinify = require("tinify");
+tinify.key = "VRX9bi9e8ZyI63fV9d4uZcc35xrvvHTq";
+
+module.exports = (robot) => {
+
+    var source = tinify.fromUrl("https://tinypng.com/images/panda-happy.png").resize({
+        method: "fit",
+        width: 100,
+        height: 100
+    });
+    // console.log(source.toFile("thumbnail.jpg"));
+    // source.toFile("thumbnail.jpg");
+    var source = tinify.fromUrl("https://tinypng.com/images/panda-happy.png");
+    console.log(source.toFile("thumbnail.jpg"));
+    source.toFile("optimized.jpg");
+    // console.log(process.cwd());
+
+    let pic = fs.readFile('./thumbnail.jpg', (err, data) => {
+        if (err) throw err;
+        console.log(data);
+    });
+
+    robot.hear(/.*/, (res) => {
+        // console.log(res);
+        // res.send(pic);
+        robot.emit('bearychat.attachment', {
+            message: res.message,
+            text: '当时我就念了...',
+            attachments: [
+                {
+                    color: '#cb3f20',
+                    text: '苟利国家生死以',
+                },
+                {
+                    text: '岂因祸福避趋之',
+                },
+                {
+                    images: [
+                        { url: pic },
+                    ]
+                }]
+        }
+        );
+    });
+
+}
