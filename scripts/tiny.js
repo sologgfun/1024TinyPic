30e207b9b32067de58d6491b65546e450cc41cb0
wangjianfeng03 <wangjianfeng03@meituan.com>
2018-10-30 14:34:23 +0800

    ok

--- scripts/tiny.js
+++ scripts/tiny.js
@@ -2,8 +2,6 @@
 //   This is a tinypic.  
 // Commands:  
 //   okbot helo - Reply with world!  
-
-const bearychat = require('bearychat');
 var fs = require("fs");
 var process = require("process");
 var tinify = require("tinify");
@@ -12,43 +10,52 @@ tinify.key = "VRX9bi9e8ZyI63fV9d4uZcc35xrvvHTq";
 
 module.exports = (robot) => {
 
-    // var source = tinify.fromUrl("https://tinypng.com/images/panda-happy.png").resize({
-    //     method: "fit",
-    //     width: 100,
-    //     height: 100
-    // });
-    // console.log(source.toFile("thumbnail.jpg"));
-    // source.toFile("thumbnail.jpg");
-    // var source = tinify.fromUrl("https://tinypng.com/images/panda-happy.png");
-    // console.log(source.toFile("thumbnail.jpg"));
-    // source.toFile("optimized.jpg");
-    console.log(process.cwd());
-
-    let pic = fs.readFile('./thumbnail.jpg', (err, data) => {
-        if (err) throw err;
-        // console.log(data);
-    });
-
-    robot.hear(/.*/, (res) => {
-        // console.log(res);
-        // res.send(pic);
+    function rename() {
+        console.log('1');
+        let nodepath = process.cwd();
+        fs.rename(`${nodepath}/new.jpg`, `${nodepath}/node_modules/hubot/src/public/new.jpg`, function (err) {
+            if (err) throw err;
+        });
         robot.emit('bearychat.attachment', {
             message: res.message,
-            text: '当时我就念了...',
+            text: 'tinypic',
             attachments: [
                 {
-                    color: '#cb3f20',
-                    text: '苟利国家生死以',
-                },
-                {
-                    text: '岂因祸福避趋之',
-                },
-                {
                     images: [
-                        { url: pic },
+                        { url: "./node_modules/hubot/src/public/tiny.jpg" },
                     ]
                 }]
-        }
-        );
+        })
+    }
+    function tinypic(url, res) {
+        // var source = tinify.fromUrl("https://tinypng.com/images/panda-happy.png");
+        var source = tinify.fromUrl(url);
+        console.log(source.toFile("new.jpg"));
+        source.toFile("new.jpg")
+            .then(rename)
+            .catch(function () {
+                robot.emit('bearychat.attachment', {
+                    message: res.message,
+                    text: 'tinypic',
+                    attachments: [
+                        {
+                            text: "please input valid image url"
+                        }]
+                })
+            });
+    };
+    // function resizepic(url, w, h) {
+    //     var source = tinify.fromUrl(url).resize({
+    //         method: "fit",
+    //         width: w,
+    //         height: h
+    //     });
+    //     source.toFile("./node_modules/hubot/src/public/resize.jpg");
+    // };
+
+    robot.hear(/.*/, (res) => {
+        var picurl = res.message.user.message.text;
+        console.log(picurl);
+        tinypic(picurl, res);
     });
 }
